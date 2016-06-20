359967245427b35cecd1ab203aa86571d9b60cb3
ikeda-motoumi <four625stars@gmail.com>
2016-06-20 17:13:18 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,15 @@
+var number = Math.floor(Math.random()*3);
+var answer = parseInt(window.prompt('かずあてげいむ 0～2のすうじをいれてね！！！！！！！！！'));
 
+var message;
+if(answer === number){
+  massage = '...ﾁｯ...';
+}else if(answer < number){
+  message = 'もっと...おおきいです...';
+}else if(answer > number){
+  message = '小者め';
+}else {
+  message = '0~3をにゅうりょくしてくださいいいいいいいいいいいいいいいいいいいいいいいいい';
+}
+
+document.getElementById('choice').textContent = message;
